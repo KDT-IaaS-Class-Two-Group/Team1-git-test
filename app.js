9ae42c48c2cdd0acff27c2893023bb35a6966c8f
KimInteger <kjs20151240@gmail.com>
2024-06-17 21:47:33 +0900

    node모듈 호출 및 server를 포트에 연결

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+const http = require('node:http');
+
+const fs = require('node:fs');
+
+const qs = require('node:querystring');
+
+const path = require('node:path');
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req,res)=>{
+  console.log('서버연결됨');
+});
+
+server.listen(PORT,(err)=>{
+  if(err){
+    throw new Error('에러가 발생했습니다. :' + err.code);
+  } else {
+    console.log('server open!');
+    console.log(`http://localhost:${PORT}`);
+  }
+});
+
 const app = "서버";
